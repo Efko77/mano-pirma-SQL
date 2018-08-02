0d66ec5653cbd33f52afca88c365901471025189
Efko77 <matavimai.evaldas@gmail.com>
2018-08-02 13:22:03 +0300

    PIRMAS PAPILDYMAS

--- src/main/java/com/ManoFirma/model/DBSingleton.java
+++ src/main/java/com/ManoFirma/model/DBSingleton.java
@@ -1,12 +1,51 @@
 package com.ManoFirma.model;
 
+import java.sql.*;
+
 public class DBSingleton {
+
+    public static final String DB_HOST_NAME = "192.168.100.100";
+    public static final String DB_PORT = "3306";
+
+    public static final String DB_NAME = "KCL";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "root";
+
+
     private static DBSingleton instance = null;
 
-    public static DBSingleton getInstance (){
-        if (instance==null){
-            instance=new DBSingleton();
+    public static DBSingleton getInstance() {
+        if (instance == null) {
+            instance = new DBSingleton();
         }
         return instance;
+    }
+
+    Connection connection = null;
+
+    private DBSingleton() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection=DriverManager.getConnection("jdbc:mysql://"+DB_HOST_NAME+":"+DB_PORT+"/"+DB_NAME, DB_USER,DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Statement statement=connection.createStatement();
+            ResultSet resultSet= statement.executeQuery("SELECT *FROM ")
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void printQueryResult(String query){
+        Statement st=null;
+
     }
 }
