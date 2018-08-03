0b3b8f554657749a0c72093a850c19c71d1204ca
Efko77 <matavimai.evaldas@gmail.com>
2018-08-03 15:10:15 +0300

    sestas PAPILDYMAS su JavaFX

--- src/main/java/com/ManoFirma/Controller/Controller.java
+++ src/main/java/com/ManoFirma/Controller/Controller.java
@@ -25,30 +25,58 @@ public class Controller {
     }
 
     public void handleButton3(ActionEvent actionEvent) {
-        String uzklausa = "SELECT * FROM `students_address` where order by `city` 'Kaunas';";
+        String uzklausa = "SELECT * FROM `student_address` where `city` = 'Kaunas';";
         String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
         isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton4(ActionEvent actionEvent) {
-        String uzklausa = "SELECT * FROM `studens` ORDER BY `name` ASC;";
+        String uzklausa = "SELECT * FROM `student_address` where `post_code` is not null;";
         String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
         isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton5(ActionEvent actionEvent) {
+        String uzklausa = "SELECT * FROM `student_address` where `post_code` is not null;";
+        String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
+        isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton6(ActionEvent actionEvent) {
+        String uzklausa = "UPDATE `student_address` SET `street` = 'Tankas' WHERE `city` IN (\n" +
+                "'Kaunas',\n" +
+                "'Vilnius',\n" +
+                "'Klaipeda'\n" +
+                ")";
+        DBSingleton.getInstance().updateQueryResult(uzklausa);
+        uzklausa = "Select * from student_address;";
+        String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
+        isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
-    public void handleButton7(ActionEvent actionEvent) {
+    public void handleButton7(ActionEvent actionEvent) { //12 but.
+        String uzklausa = "SELECT * FROM `studens` LEFT JOIN `student_address`\n" +
+                "ON `studens`.`id` = `student_address`.`student_id` order by `name`";
+        String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
+        isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton8(ActionEvent actionEvent) {
+        String uzklausa = "SELECT * FROM `studens` LEFT JOIN `student_marks`\n" +
+                "ON `studens`.`id` = `student_marks`.`student_id`ORDER BY `mark`";
+        String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
+        isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton9(ActionEvent actionEvent) {
+        String uzklausa = "SELECT *\n" +
+                "FROM `studens`\n" +
+                "INNER JOIN `student_address` ON `studens`.`id` = `student_address`.`student_id`\n" +
+                "INNER JOIN `student_marks` ON `studens`.`id` = `student_marks`.`student_id`\n" +
+                "WHERE `city` = 'Kaunas'\n" +
+                "ORDER BY `name` LIKE '%as'";
+        String atsakymas = DBSingleton.getInstance().printQueryResult(uzklausa);
+        isvedimoLaukas.appendText(atsakymas + "\n");
     }
 
     public void handleButton10(ActionEvent actionEvent) {
